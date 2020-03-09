f38d82490000738dc524471c2926ac9aec3a1ca1
Alan <masteralan2001@gmail.com>
2020-03-09 10:52:22 -0400

    Added basic Vector class

---
+++ Engines/Vector.js
@@ -0,0 +1,26 @@
+class Vector {  // A structure that holds position data or direction and magnitude
+    constructor(x, y, z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    // Returns length of this vector (or should this be static?)
+    get length() {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2) + Math.pow(this.z, 2));
+    }
+
+    // Static methods--generates new vectors to avoid overriding old properties
+    static Add(a, b) {
+        return new Vector(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+    static Subtract(a, b) {
+        return new Vector(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    // Returns a copy of the given vector with the same direction but a length of one
+    static UnitVector(a) {
+        const len = a.length();
+        return new Vector(a.x/len, a.y/len, a.z/len);
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Luchadores
 
 Luchadores is an arena-style combat game with a cast of silly and weird animals that participate in exaggerated "wrestling" matches.
+
+Design Document
+https://docs.google.com/document/d/1MlaYaygWPhbE7BNI9D7iT2IaouNDpXv2TJxrf_YoSdo/
