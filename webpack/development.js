29a80ef445e96a2b2030fad787b6ff45da2dd123
Max O'Cull <max.ocull@protonmail.com>
2020-03-09 22:16:16 -0400

    Fix index.html

--- webpack/development.js
+++ webpack/development.js
@@ -27,9 +27,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
-    }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
       WEBGL_RENDERER: JSON.stringify(true)
