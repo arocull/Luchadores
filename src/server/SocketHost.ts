260f446287616aeff10618c1972fc5cb7f5035aa
James O'Cull <james.ocull@gmail.com>
2020-03-14 16:04:36 -0400

    Log when new websocket connections come in

--- src/server/SocketHost.ts
+++ src/server/SocketHost.ts
@@ -1,13 +1,17 @@
 import * as http from 'http';
 import * as socketIo from 'socket.io';
 
-// import logger from './Logger';
+import logger from './Logger';
 
 class SocketHost {
   public ws: socketIo.Server;
 
   constructor(server: http.Server) {
     this.ws = socketIo(server);
+
+    this.ws.on('connection', (socket) => {
+      logger.info(`New websocket connection! ${socket.id}`);
+    });
   }
 }
 
