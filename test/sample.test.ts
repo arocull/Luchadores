d3207e3403bb77c5d13c02961e0c1b8e6d81139a
James O'Cull <james.ocull@gmail.com>
2020-03-14 14:29:47 -0400

    Add a simple lodash test for defaults

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -1,4 +1,10 @@
-// any imports go here
+import * as _ from 'lodash';
+
+interface MyInterface {
+  Host: string;
+  Port: number;
+  Enabled: boolean;
+}
 
 function sleep(ms: number) {
   return new Promise((resolve) => {
@@ -9,3 +15,15 @@ function sleep(ms: number) {
 test('simple async test', async () => {
   await sleep(250);
 }, 2000);
+
+test('defaults deep test', () => {
+  let opt : MyInterface = _.defaultsDeep({}, {
+    Host: '127.0.0.1',
+    Port: 1337,
+    Enabled: false
+  });
+
+  expect(opt.Host).toBe('127.0.0.1');
+  expect(opt.Port).toBe(1337);
+  expect(opt.Enabled).toBe(false);
+});
