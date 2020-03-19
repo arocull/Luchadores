810cf042388015c5e7fa91e33ff9d4babd83ebd2
James O'Cull <james.ocull@gmail.com>
2020-03-18 21:16:08 -0400

    Added foundation of message decoder

---
+++ src/common/messaging/decoder.ts
@@ -0,0 +1,20 @@
+import * as events from '../events/events';
+
+/**
+ * Decodes the provided envelope into its concrete type
+ */
+function decode(envelope: events.core.Envelope): any {
+  switch (envelope.type) {
+    case events.core.TypeEnum.LobbyRequest:
+      return events.lobby.LobbyRequest.decode(envelope.data);
+    case events.core.TypeEnum.LobbyResponse:
+      return events.lobby.LobbyResponse.decode(envelope.data);
+    default:
+      // TODO: Figure out how to make this an exhaustive switch
+      // and produce a compile error if not all cases are covered.
+      // https://www.typescriptlang.org/docs/handbook/advanced-types.html#exhaustiveness-checking
+      return null as never;
+  }
+}
+
+export { decode as default };
