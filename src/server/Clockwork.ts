f1d4219bca760de7e460a513744428b9790d74fe
James O'Cull <james.ocull@gmail.com>
2020-04-25 15:32:41 -0400

    Run Clockwork at a fixed rate (20 ticks per second)

--- src/server/Clockwork.ts
+++ src/server/Clockwork.ts
@@ -29,6 +29,7 @@ class Clockwork {
   private connections: Player[] = [];
   private world: World;
   private running: boolean = false;
+  private tickRate: number;
   private actions: Denque<Action>;
   private loop: NodeJS.Timeout;
   private subscribers: SubscriberContainer;
@@ -39,22 +40,13 @@ class Clockwork {
 
     this.actions = new Denque<Action>();
     this.subscribers = new SubscriberContainer();
+    this.tickRate = 1000 / 20; // Number of milliseconds per tick (tick rate = 20 per second)
   }
 
   tick(delta: number) {
     this.loop = null;
 
     if (this.running) {
-      // TODO: Could just be a reduce function for worst ping in the future
-      const pings: number[] = _.map(this.connections, (conn) => conn.getPing());
-
-      // Worst of *average* pings! This way we even the playing field so the
-      // player with fiber doesn't whoop on the guy with dialup.
-      // Math.max prevents ping rounding error from ever being negative.
-      // Defaults using || 20.
-      const worstPing = Math.max(0, _.max(pings)) || 20;
-      Logger.silly('Worst ping: %j, %o', pings, worstPing);
-
       if (delta > 0) {
         const actionArray = this.actions.toArray();
         this.actions.clear(); // Reset the list of actions for next tick
@@ -81,8 +73,9 @@ class Clockwork {
         });
 
         // Get remainder time
-        const remainder = worstPing - span;
+        const remainder = this.tickRate - span;
         this.world.tick(remainder / 1000); // Decimal seconds
+        // Logger.debug('Ticked %o actions at %o with remainder %o', actionArray.length, Date.now(), remainder);
 
         // Finally, update world state for all clients
         this.broadcast(encodeWorldState(this.world)); // Encodes and passes the full world-state as a message
@@ -105,7 +98,7 @@ class Clockwork {
 
       // Kick off the interval.
       // Keep the local context by using an arrow function.
-      this.loop = setTimeout(() => this.tick(worstPing), worstPing);
+      this.loop = setTimeout(() => this.tick(this.tickRate), this.tickRate);
     }
   }
 
