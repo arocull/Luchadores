9226f5e2c2cfa070eb504ccc9164c4948d02b0b8
James O'Cull <james.ocull@gmail.com>
2020-04-25 14:54:08 -0400

    Revert Random to use Date.now() and document why

--- src/common/engine/Random.ts
+++ src/common/engine/Random.ts
@@ -1,5 +1,3 @@
-import { Timer } from './time/Time';
-
 let seed = 1; // Seed used by random number generator
 let index = 0; // Number of times a random number has been generated on this seed
 
@@ -16,7 +14,10 @@ class Random {
 
   // Creates a random seed based off of current time (randomized as well by previous use if there was any)
   static randomSeed() {
-    this.setSeed(Timer.now() + index * seed);
+    // Use Date.now here instead of Timer.now to be sure that we don't always supply `0` on application start.
+    // We do this because the Timer's clock is based on an origin time, which might always begin at zero,
+    // depending on when the application starts and selects an origin time.
+    this.setSeed(Date.now() + index * seed);
   }
 
   // Returns a random float between 0 and 1
