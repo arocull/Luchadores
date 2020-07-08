94cd5111f7a66d9078445c37c3cdede1427d2300
Alan O'Cull <masteralan2001@gmail.com>
2020-07-08 16:43:50 -0400

    WIP: ClientState module

---
+++ src/client/network/ClientState.ts
@@ -0,0 +1,379 @@
+/* eslint-disable object-curly-newline */
+/* eslint-disable padded-blocks */
+import _ from 'lodash';
+import { sampleInputs, PlayerInput, Topics as InputTopics, KeyboardButtonInput } from '../controls/playerinput';
+import NetworkClient from './client';
+import { MessageBus, Topics as BusTopics } from '../../common/messaging/bus';
+import { SubscriberContainer } from '../../common/messaging/container';
+import { decodeInt64 } from '../../common/messaging/serde';
+import { IEvent, TypeEnum } from '../../common/events/index';
+import { IPlayerConnect, IPlayerState, IWorldState, IPlayerListState, WorldState } from '../../common/events/events';
+import decodeWorldState from '../network/WorldStateDecoder';
+import World from '../../common/engine/World';
+import Player from '../../common/engine/Player';
+import Fighter from '../../common/engine/Fighter';
+import Random from '../../common/engine/Random';
+import Camera from '../../client/Camera';
+import { MapPreset, FighterType } from '../../common/engine/Enums';
+import { UIDeathNotification, UIPlayerInfo } from '../ui';
+import UIManager from '../ui/UIManager';
+import { Vector } from '../../common/engine/math';
+import InputState from '../../client/controls/InputState';
+/* eslint-enable object-curly-newline */
+
+class Client {
+  // Player and Character objects
+  public player: Player;
+  public character: Fighter;
+
+  // Respawn info
+  public respawnTimer: number;
+  public respawning: boolean;
+
+  // Networking
+  public connected: boolean;
+  private playerList: Player[];
+  private topics: any;
+
+  // Inputs
+  public input: InputState;
+  private inputSubscribers: SubscriberContainer;
+
+  // World Data
+  private world: World;
+  private worldUpdatePending: boolean; // Is there a WorldState update that is pending
+  private worldUpdateLastPacketTime: number; // Time the world state packet was received
+  private worldUpdate: IWorldState; // WorldState event
+  private worldUpdateFirst: boolean; // Is this the first WorldState the client is receiving (applied map prop loading)?
+
+  // Render Hook-Ups, contain some render data but also important information
+  private screenWidth: number;
+  private screenHeight: number;
+  public camera: Camera;
+  private uiPlayerList: UIPlayerInfo[];
+  private uiDeathNotifs: UIDeathNotification[];
+  public uiManager: UIManager; // Hook-up, should not be auto-generated
+
+  constructor() {
+    this.player = new Player('');
+    this.character = null;
+
+    this.respawnTimer = 3; // Counts up from 0 until player is spawning
+    this.respawning = false; // True if player has selected character and is waiting on assignment from server
+
+    this.connected = false;
+    this.topics = { // TODO: HAX - get topics to use from web socket
+      ClientNetworkToServer: null as string,
+      ClientNetworkFromServer: null as string,
+    };
+
+    this.input = new InputState();
+    this.inputSubscribers = null;
+
+    // Initialize basic, empty world for player to roam around in if we use it
+    // Is populated and changed when player connects
+    this.world = new World(MapPreset.Grassy, false, true);
+    Random.randomSeed();
+
+    this.worldUpdatePending = false;
+    this.worldUpdateLastPacketTime = 0;
+    this.worldUpdate = null;
+    this.worldUpdateFirst = true;
+
+    // Initialize render hookups (implemented by ClientGraphics module)
+    this.screenWidth = 600;
+    this.screenHeight = 400;
+    this.camera = new Camera(this.screenWidth, this.screenHeight, 18, 14);
+    this.camera.SetFocusPosition(new Vector(this.world.Map.Width / 2, this.world.Map.Height / 2, 0));
+    this.uiPlayerList = [];
+    this.uiDeathNotifs = [];
+    this.uiManager = null;
+
+
+    // Set up message bus events
+    MessageBus.subscribe('PickUsername', (name: string) => {
+      this.player.setUsername(name);
+
+      MessageBus.publish(this.topics.ClientNetworkToServer, <IPlayerConnect>{
+        type: TypeEnum.PlayerConnect,
+        ownerId: -1, // We don't know this on this client yet - server will decide
+        username: name,
+      });
+
+      if (this.uiManager) {
+        this.uiManager.closeUsernameSelect();
+        this.uiManager.openClassSelect();
+
+        this.uiPlayerList.push(new UIPlayerInfo(this.player, true));
+      }
+    });
+    MessageBus.subscribe('PickCharacter', (type: FighterType) => {
+      this.respawnTimer = 3;
+      this.respawning = true;
+
+      if (this.uiManager) this.uiManager.closeClassSelect();
+
+      MessageBus.publish(this.topics.ClientNetworkToServer, {
+        type: TypeEnum.PlayerSpawned,
+        fighterClass: type,
+      });
+    });
+  }
+
+
+  /* Player Connection Events */
+
+  // Call when a player joins--adds them to the player list and creates player info slip
+  private onPlayerConnect(plr: Player) {
+    this.playerList.push(plr);
+    this.uiPlayerList.push(new UIPlayerInfo(plr));
+  }
+  // Call when a player leaves--removes from player list and remove their info slip
+  private onPlayerDisconnect(plr: Player) {
+    for (let i = 0; i < this.playerList.length; i++) {
+      if (this.playerList[i] === plr) {
+        this.playerList.splice(i, 1);
+        break;
+      }
+    }
+    for (let i = 0; i < this.uiPlayerList.length; i++) {
+      if (this.uiPlayerList[i].getOwner() === plr) {
+        this.uiPlayerList.splice(i, 1);
+        return;
+      }
+    }
+  }
+  // Takes a player list state provided by server and deciphers it
+  private onPlayerListUpdate(msg: IPlayerListState) {
+    this.player.assignCharacterID(msg.characterId);
+
+    for (let i = 0; i < this.playerList.length; i++) {
+      this.playerList[i].accountedFor = false;
+    }
+
+    for (let i = 0; i < msg.players.length; i++) {
+      const id = msg.players[i].ownerId;
+      let plr = null;
+      let isNew: boolean = false;
+
+      if (id === msg.characterId) plr = this.player;
+      else {
+        for (let j = 0; j < this.playerList.length; j++) {
+          if (id === this.playerList[j].getCharacterID()) {
+            plr = this.playerList[j];
+            break;
+          }
+        }
+        if (!plr) { // If player does not exist in current list, create them
+          plr = new Player(`Player${id}`);
+          isNew = true;
+        }
+      }
+
+      // Update player info
+      plr.setUsername(msg.players[i].username);
+      plr.assignCharacterID(id);
+      plr.setKills(msg.players[i].kills);
+      // TODO: Set max killstreak and/or current killstreak?
+      plr.updatePing(msg.players[i].averagePing);
+      plr.accountedFor = true;
+
+      if (isNew) this.onPlayerConnect(plr);
+    }
+
+    // Prune players that were not accounted for
+    for (let i = 0; i < this.playerList.length; i++) {
+      if (!this.playerList[i].accountedFor) {
+        this.onPlayerDisconnect(this.playerList[i]);
+        i--;
+      }
+    }
+  }
+  // Returns player for the corresponding character ID
+  private getPlayerFromCharacterID(characterID: number): Player {
+    for (let i = 0; i < this.playerList.length; i++) {
+      if (this.playerList[i] && this.playerList[i].getCharacterID() === characterID) {
+        return this.playerList[i];
+      }
+    }
+    return null; // No player object found, return null
+  }
+
+
+  /* Death and Kill Management */
+
+  // Calls when a player is killed
+  private onDeath(died: number, killer: number) {
+    let diedName: string = '';
+    let killerCharacter: Fighter = null;
+
+    for (let i = 0; i < this.world.Fighters.length; i++) { // Iterate through all fighters
+
+      if (this.world.Fighters[i].getOwnerID() === died) { // Character is killed
+        // Burst confetti PConfetti.Burst(particles, world.Fighters[i].Position, 0.2, 4, 50 * RenderSettings.ParticleAmount)
+        diedName = this.world.Fighters[i].DisplayName; // Obtain this honorable luchador's name
+        this.world.Fighters[i].MarkedForCleanup = true; // Mark for cleanup (allows kill counting before removal)
+
+        // Do not remove character from player who died until cleanup
+        // const diedPlayer = this.getPlayerFromCharacterID(died);
+        // if (diedPlayer) diedPlayer.removeCharacter(); // Remove character from player who died
+
+      } else if (this.world.Fighters[i].getOwnerID() === killer) { // Character that did it
+        killerCharacter = this.world.Fighters[i]; // Keep track of killer (for killcam)
+        killerCharacter.EarnKill(); // Earn kill and perform kill reward functions
+        if (killerCharacter.Animator) killerCharacter.Animator.killEffectCountdown = 1; // Timer for rose petal shower
+
+        const killerPlayer = this.getPlayerFromCharacterID(killer);
+        if (killerPlayer) killerPlayer.earnKill(); // Count kill for killer's player
+      }
+    }
+
+    if (died === this.player.getCharacterID()) { // Death effects and killcam
+      this.character = null;
+      if (killerCharacter) {
+        this.camera.LerpToFocus(killerCharacter);
+        if (this.uiManager) this.uiManager.playerDied(`Killed by ${killerCharacter.DisplayName}`);
+      } else if (this.uiManager) {
+        this.uiManager.playerDied();
+      }
+    }
+
+    if (killerCharacter) { // Show specific death message
+      this.uiDeathNotifs.push(new UIDeathNotification(
+        diedName,
+        killerCharacter.DisplayName,
+        killerCharacter.getCharacter(),
+        died === this.player.getCharacterID(),
+        killer === this.player.getCharacterID(),
+      ));
+    } else { // Show generic death message
+      this.uiDeathNotifs.push(new UIDeathNotification(
+        diedName,
+        null,
+        FighterType.None,
+        died === this.player.getCharacterID(),
+        false,
+      ));
+    }
+
+    // Rank player list by kills
+    this.uiPlayerList.sort(UIPlayerInfo.SORT);
+  }
+
+
+  /* User Input */
+  private parseKeys(input: PlayerInput) {
+    if (this.uiManager === null) return; // Do not attempt to parse input if no UI manager present
+
+    // Type into username textbox
+    if (this.uiManager.inGUIMode() && this.inputSubscribers == null) {
+      // When we enter GUI mode, bind the events
+      this.inputSubscribers = new SubscriberContainer();
+      this.inputSubscribers.attach(InputTopics.keydown, (k: KeyboardButtonInput) => {
+        this.uiManager.keyInput(k.key, k.shiftKey);
+      });
+    } else if (!this.uiManager.inGUIMode() && this.inputSubscribers != null) {
+      // When we leave GUI mode, unbind the events
+      this.inputSubscribers.detachAll();
+    }
+
+    if (input.Keys.a === true) this.input.MoveDirection.x = -1;
+    else if (input.Keys.d === true) this.input.MoveDirection.x = 1;
+    else this.input.MoveDirection.x = 0;
+
+    if (input.Keys.w === true) this.input.MoveDirection.y = 1;
+    else if (input.Keys.s === true) this.input.MoveDirection.y = -1;
+    else this.input.MoveDirection.y = 0;
+
+    this.input.Jump = (input.Keys[' '] === true);
+    this.uiManager.togglePlayerList(input.Keys.y === true);
+  }
+  private parseMouse(input: PlayerInput) {
+    // Button 0 is left click
+    this.input.MouseDown = (input.MouseButtons[0] === true);
+
+    this.input.MouseX = input.MouseCoordinates.x;
+    this.input.MouseY = input.MouseCoordinates.y;
+
+    if (this.character && this.character.isRanged() && this.input.MouseDown) {
+      // If the character is present, we should grab mouse location based off of where projectiles are likely to be fired
+      const dir = Vector.UnitVectorXY(
+        Vector.Subtract(
+          this.camera.PositionOffset(this.character.Position),
+          input.MouseCoordinates,
+        ),
+      );
+      dir.x *= -1;
+      this.input.MouseDirection = dir;
+    } else { // Otherwise, do simple direction calculation
+      this.input.MouseDirection = Vector.UnitVectorFromXYZ(
+        input.MouseCoordinates.x - (this.screenWidth / 2),
+        (this.screenHeight / 2) - input.MouseCoordinates.y,
+        0,
+      );
+    }
+  }
+  private scrapeInput() {
+    const inp = sampleInputs();
+    this.parseKeys(inp);
+    this.parseMouse(inp);
+
+    // Do not send input updates if the player is fiddling with UI
+    if (this.uiManager && (this.uiManager.inGUIMode() || !this.character)) return;
+
+    MessageBus.publish(this.topics.ClientNetworkToServer, {
+      type: TypeEnum.PlayerInputState,
+      jump: this.input.Jump,
+      mouseDown: this.input.MouseDown,
+      mouseDirection: this.input.MouseDirection,
+      moveDirection: this.input.MoveDirection,
+    });
+  }
+
+
+  /* State Updates */
+  private updatePlayerState(msg: IPlayerState) {
+    const mismatch = msg.characterID !== this.player.getCharacterID();
+
+    this.player.assignCharacterID(msg.characterID);
+
+    if (mismatch && this.character) { // Kill character if there was an ID mismatch
+      this.character.HP = 0;
+      this.character.LastHitBy = null;
+      this.character.MarkedForCleanup = true;
+    }
+
+    if (this.character) this.character.HP = msg.health;
+  }
+
+
+  /* Render Hookup */
+  public scaleScreen(newWidth: number, newHeight: number) {
+    this.screenWidth = newWidth;
+    this.screenHeight = newHeight;
+    this.camera.Scale(newWidth, newHeight);
+  }
+
+
+  /* Tick */
+  // Updates time by X seconds
+  public doFrame(DeltaTime: number) {
+    let appliedWorldState = false;
+
+    this.scrapeInput(); // Capture inputs (updates this.input)
+
+    // Prune fighters that have died
+    for (let i = 0; i < this.world.Fighters.length; i++) {
+      if (this.world.Fighters[i].MarkedForCleanup) {
+        const plr = this.getPlayerFromCharacterID(this.world.Fighters[i].getOwnerID());
+        if (plr) plr.removeCharacter(); // Remove player character
+
+        this.world.Fighters.splice(i, 1);
+        i--;
+      }
+    }
+  }
+}
+/* eslint-enable padded-blocks */
+
+export { Client as default };
