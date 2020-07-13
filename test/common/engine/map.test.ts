b0430e4ae6507e909ceccda11e694f021f456474
Alan O'Cull <masteralan2001@gmail.com>
2020-07-13 11:35:22 -0400

    Fixed Map test (texture management changed)

--- test/common/engine/map.test.ts
+++ test/common/engine/map.test.ts
@@ -1,12 +1,9 @@
-import Map from '../../../src/common/engine/Map';
 import World from '../../../src/common/engine/World';
 import { MapPreset } from '../../../src/common/engine/Enums';
 
 test('map preset test', () => {
-  const world = new World(MapPreset.Sandy, true, true);
-  const map: Map = world.Map;
+  const world = new World(MapPreset.Sandy, true, false);
 
   expect(world.Props.length).toBeGreaterThan(0); // Did the world load in props?
-  expect(world.Props[0].texture).toBeTruthy(); // Did the props get textures?
-  expect(map.Texture).toBeTruthy(); // Did the map get a texture?
+  expect(world.Map); // Expect map to exist
 });
