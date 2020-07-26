f0a525c40ca08e5ca43f298ae44518f72bfa8d22
James O'Cull <james.ocull@gmail.com>
2020-07-26 19:13:11 -0400

    Refactoring and clean up

--- src/server/Host.ts
+++ src/server/Host.ts
@@ -23,61 +23,79 @@ class Host extends EventEmitter {
 
   initialize() {
     this.webHost.initialize();
+    this.initializeHeartbeatServers();
 
-    const connectionCountChange = () => {
-      if (this.clockwork && this.socketHost.getClients().length === 0) {
-        // Wait a moment to receive any residual packets, or maybe a player will
-        // refill the spot.
-        setTimeout(() => {
-          // Make sure our conditions still hold.
-          if (this.clockwork && this.socketHost.getClients().length === 0) {
-            logger.info('Shutting down game instance ...');
-            this.clockwork.stop();
-            this.clockwork = null;
-          }
-        }, 5 * 1000);
-      } else if (!this.clockwork && this.socketHost.getClients().length > 0) {
-        logger.info('Spinning up game instance ...');
-        this.clockwork = new Clockwork();
-        this.clockwork.start();
-      }
-    };
+    this.socketHost.on('connect', () => this.onConnectionCountChange());
+    this.socketHost.on('disconnect', () => this.onConnectionCountChange());
+  }
 
-    this.socketHost.on('connect', connectionCountChange);
-    this.socketHost.on('disconnect', connectionCountChange);
-
+  private initializeHeartbeatServers() {
     // Report to heartbeat server(s).
     // We do this even if the game instance is not running because it *could*
     // be running if someone would just connect.
     const servers = config.get<string[]>('heartbeat.servers') || [];
-    if (!Array.isArray(servers) || typeof servers[0] !== 'string') {
+    if (!Array.isArray(servers)) {
       logger.error('Heartbeat servers is misconfigured as %j', servers);
-    } else {
-      interface HeartbeatData {
-        title: string;
-        address: string;
-        playerCount: number;
-        playerCapacity: number;
-      }
-      const addresses = servers.map((baseUrl) => new URL('/heartbeat', baseUrl).toString());
-      setInterval(() => {
-        const heartbeatData: HeartbeatData = {
-          title: config.get<string>('server.title'),
-          address: config.get<string>('server.address'),
-          playerCount: this.getPlayerCount(),
-          playerCapacity: this.getPlayerCapacity(),
-        };
+      return;
+    }
+    if (servers.length === 0) {
+      logger.warn('No heartbeat servers configured - this server will not announce itself');
+      return;
+    }
+    if (typeof servers[0] !== 'string') {
+      logger.error('Heartbeat servers is misconfigured as %j', servers);
+      return;
+    }
 
-        addresses.forEach((address) => {
-          axios.post(address, heartbeatData)
-            .then(() => {
-              logger.debug('Reporting to %o was OK', address);
-            })
-            .catch((err) => {
-              logger.error('Failed to report to %o: %j', address, err);
-            });
-        });
-      }, 10 * 1000); // TODO: Re-evaluate this reporting interval later
+    interface HeartbeatData {
+      title: string;
+      address: string;
+      playerCount: number;
+      playerCapacity: number;
+    }
+
+    const addresses = servers.map((baseUrl) => new URL('/heartbeat', baseUrl).toString());
+    const fnHeartbeat = () => {
+      const heartbeatData: HeartbeatData = {
+        title: config.get<string>('server.title'),
+        address: config.get<string>('server.address'),
+        playerCount: this.getPlayerCount(),
+        playerCapacity: this.getPlayerCapacity(),
+      };
+
+      addresses.forEach((address) => {
+        axios.post(address, heartbeatData)
+          .then(() => {
+            logger.silly('Heartbeat reporting to %o was OK', address);
+          })
+          .catch((err) => {
+            logger.error('Heartbeat reporting to %o failed: %j', address, err);
+          });
+      });
+    };
+
+    // Run once now, and then keep running on an interval
+    // TODO: Re-evaluate this reporting interval later
+    fnHeartbeat();
+    setInterval(fnHeartbeat, 10 * 1000);
+  }
+
+  private onConnectionCountChange() {
+    if (this.clockwork && this.socketHost.getClients().length === 0) {
+      // Wait a moment to receive any residual packets, or maybe a player will
+      // refill the spot.
+      setTimeout(() => {
+        // Make sure our conditions still hold.
+        if (this.clockwork && this.socketHost.getClients().length === 0) {
+          logger.info('Shutting down game instance ...');
+          this.clockwork.stop();
+          this.clockwork = null;
+        }
+      }, 5 * 1000);
+    } else if (!this.clockwork && this.socketHost.getClients().length > 0) {
+      logger.info('Spinning up game instance ...');
+      this.clockwork = new Clockwork();
+      this.clockwork.start();
     }
   }
 
