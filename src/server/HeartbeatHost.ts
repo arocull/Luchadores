8300e7a2f6fd13efcb67939792a2ba346ce7c45f
James O'Cull <james.ocull@gmail.com>
2020-08-08 11:32:07 -0400

    Initial work refactoring HeartbeatHost

---
+++ src/server/HeartbeatHost.ts
@@ -0,0 +1,87 @@
+import { URL } from 'url';
+
+import axios from 'axios';
+import config from 'config';
+import { Express } from 'express';
+
+import Clockwork from './Clockwork';
+import logger from './Logger';
+
+class HeartbeatHost {
+  constructor(
+    private app: Express,
+    private fnGetClockwork: () => Clockwork,
+  ) {
+  }
+
+  initialize() {
+    this.initializeHeartbeatServers();
+  }
+
+  private getPlayerCount(): number {
+    const clockwork = this.fnGetClockwork();
+    if (clockwork) {
+      return clockwork.getPlayerCount();
+    }
+    return 0; // No server instance running
+  }
+
+  private getPlayerCapacity(): number {
+    // TODO: Implement an actual capacity.
+    //       See issue #75 in Gitlab
+    return 20;
+  }
+
+  private initializeHeartbeatServers() {
+    // Report to heartbeat server(s).
+    // We do this even if the game instance is not running because it *could*
+    // be running if someone would just connect.
+    const servers = config.get<string[]>('heartbeat.servers') || [];
+    if (!Array.isArray(servers)) {
+      logger.error('Heartbeat servers is misconfigured as %j', servers);
+      return;
+    }
+    if (servers.length === 0) {
+      logger.warn('No heartbeat servers configured - this server will not announce itself');
+      return;
+    }
+    if (typeof servers[0] !== 'string') {
+      logger.error('Heartbeat servers is misconfigured as %j', servers);
+      return;
+    }
+
+    interface HeartbeatData {
+      title: string;
+      address: string;
+      playerCount: number;
+      playerCapacity: number;
+    }
+
+    const addresses = servers.map((baseUrl) => new URL('/heartbeat', baseUrl).toString());
+    const fnHeartbeat = () => {
+      const heartbeatData: HeartbeatData = {
+        title: config.get<string>('server.title'),
+        address: config.get<string>('server.address'),
+        playerCount: this.getPlayerCount(),
+        playerCapacity: this.getPlayerCapacity(),
+      };
+
+      addresses.forEach((address) => {
+        axios.post(address, heartbeatData)
+          .then(() => {
+            logger.silly('Heartbeat reporting to %s was OK', address);
+          })
+          .catch((err) => {
+            logger.error('Heartbeat reporting to %s failed: %j', address, err);
+          });
+      });
+    };
+
+    // Run once now, and then keep running on an interval
+    // TODO: Re-evaluate this reporting interval later
+    fnHeartbeat();
+    setInterval(() => fnHeartbeat(), 10 * 1000);
+  }
+}
+
+export { HeartbeatHost as default };
--- src/server/Host.ts
+++ src/server/Host.ts
@@ -1,15 +1,13 @@
 import { EventEmitter } from 'events';
-import { URL } from 'url';
 
-import axios from 'axios';
-import config from 'config';
-
+import HeartbeatHost from './HeartbeatHost';
 import logger from './Logger';
 import SocketHost from './SocketHost';
 import WebHost from './WebHost';
 import Clockwork from './Clockwork';
 
 class Host extends EventEmitter {
+  private heartbeatHost: HeartbeatHost;
   private socketHost: SocketHost;
   private webHost: WebHost;
   private clockwork: Clockwork;
@@ -19,65 +17,19 @@ class Host extends EventEmitter {
 
     this.webHost = new WebHost(port);
     this.socketHost = new SocketHost(this.webHost.http);
+    this.heartbeatHost = new HeartbeatHost(this.webHost.app, () => this.getClockwork());
   }
 
   initialize() {
     this.webHost.initialize();
-    this.initializeHeartbeatServers();
+    this.heartbeatHost.initialize();
 
     this.socketHost.on('connect', () => this.onConnectionCountChange());
     this.socketHost.on('disconnect', () => this.onConnectionCountChange());
   }
 
-  private initializeHeartbeatServers() {
-    // Report to heartbeat server(s).
-    // We do this even if the game instance is not running because it *could*
-    // be running if someone would just connect.
-    const servers = config.get<string[]>('heartbeat.servers') || [];
-    if (!Array.isArray(servers)) {
-      logger.error('Heartbeat servers is misconfigured as %j', servers);
-      return;
-    }
-    if (servers.length === 0) {
-      logger.warn('No heartbeat servers configured - this server will not announce itself');
-      return;
-    }
-    if (typeof servers[0] !== 'string') {
-      logger.error('Heartbeat servers is misconfigured as %j', servers);
-      return;
-    }
-
-    interface HeartbeatData {
-      title: string;
-      address: string;
-      playerCount: number;
-      playerCapacity: number;
-    }
-
-    const addresses = servers.map((baseUrl) => new URL('/heartbeat', baseUrl).toString());
-    const fnHeartbeat = () => {
-      const heartbeatData: HeartbeatData = {
-        title: config.get<string>('server.title'),
-        address: config.get<string>('server.address'),
-        playerCount: this.getPlayerCount(),
-        playerCapacity: this.getPlayerCapacity(),
-      };
-
-      addresses.forEach((address) => {
-        axios.post(address, heartbeatData)
-          .then(() => {
-            logger.silly('Heartbeat reporting to %s was OK', address);
-          })
-          .catch((err) => {
-            logger.error('Heartbeat reporting to %s failed: %j', address, err);
-          });
-      });
-    };
-
-    // Run once now, and then keep running on an interval
-    // TODO: Re-evaluate this reporting interval later
-    fnHeartbeat();
-    setInterval(() => fnHeartbeat(), 10 * 1000);
+  getClockwork(): Clockwork {
+    return this.clockwork;
   }
 
   private onConnectionCountChange() {
@@ -97,19 +49,6 @@ class Host extends EventEmitter {
       this.clockwork = new Clockwork();
       this.clockwork.start();
     }
-  }
-
-  private getPlayerCount(): number {
-    if (this.clockwork) {
-      return this.clockwork.getPlayerCount();
-    }
-    return 0; // No server instance running
-  }
-
-  private getPlayerCapacity(): number {
-    // TODO: Implement an actual capacity.
-    //       See issue #75 in Gitlab
-    return 20;
   }
 }
 
